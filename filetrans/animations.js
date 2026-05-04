e519dc22d4d1effda06602c8abd9575ceb2b1dff
alexnamurinda <namurindaalex44@gmail.com>
2026-05-04 13:17:40 +0300

    final edits

--- filetrans/animations.js
+++
@@ -1,9 +0,0 @@
-// Initialize AOS (Animate On Scroll)
-document.addEventListener('DOMContentLoaded', function() {
-    AOS.init({ 
-        duration: 800, 
-        once: true, 
-        offset: 80, 
-        easing: 'ease-out-cubic' 
-    });
-});
